fbeb08b6ed43055425ea52e7073aa34548f425bb
Afsha <jafsha71@gmail.com>
2025-12-07 21:40:58 +0530

    updated practise loops

---
+++ src/HOMEWORKS/practiseloops2.java
@@ -0,0 +1,29 @@
+package HOMEWORKS;
+
+public class practiseloops2 {
+    public static void main(String[] args) {
+        // 1-5 in for loop
+        for(int p = 1; p<=5 ; p++){
+            System.out.print(p + " ");
+        }
+        // 1-5 in while loop
+        int i = 1;
+        while(i<= 5){
+            System.out.print(i + " ");
+            i++;
+        }
+
+        // 1- 5 in do-while
+        int s = 1;
+        do{
+            System.out.print(s + " ");
+            s++;
+        }
+        while(s<=5);
+
+        // menu driven program
+        
+
+
+    }
+}
