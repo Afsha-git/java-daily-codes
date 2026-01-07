1ff0da940294b2777ca7744ded8ba9a215af9ec5
Afsha <jafsha71@gmail.com>
2026-01-07 18:55:52 +0530

    add more loop programs

---
+++ src/JAVATOPCODES/patterns/ButterflyPattern.java
@@ -0,0 +1,29 @@
+package JAVATOPCODES.patterns;
+
+public class ButterflyPattern {
+    public static void main(String[] args) {
+        int n = 8;
+        //outer loop
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("*");
+            }
+            for (int j = i; j <=n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("*");
+            }
+            for (int j = i; j <=n; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <=n ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/JAVATOPCODES/practise/FibonacciSeries.java
@@ -0,0 +1,13 @@
+package JAVATOPCODES.practise;
+/*Each number is the sum of the previous two numbers.*/
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = 10,a=0,b=1;
+        for (int i = 1; i <=n ; i++) {
+            System.out.print(a + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
