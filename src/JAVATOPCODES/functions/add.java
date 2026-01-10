cfdf7d0c22e3ec9e10389f66bccec30c14b2ac29
Afsha <jafsha71@gmail.com>
2026-01-10 23:26:59 +0530

    add sum function in java

---
+++ src/JAVATOPCODES/functions/add.java
@@ -0,0 +1,10 @@
+package JAVATOPCODES.functions;
+
+public class add {
+    static int sum(int a , int b){
+        return a + b;
+    }
+    public static void main(String[] args) {
+        System.out.println(" sum of two intergers is " + sum(10 , 20));
+    }
+}
